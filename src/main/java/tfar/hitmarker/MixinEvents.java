7f990a1ec013649c4997f270f834415fcda76580
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2021-12-12 12:49:46 -0500

    1.18.1

--- src/main/java/tfar/hitmarker/MixinEvents.java
+++ src/main/java/tfar/hitmarker/MixinEvents.java
@@ -1,5 +1,6 @@
 package tfar.hitmarker;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
@@ -10,12 +11,13 @@ public class MixinEvents {
     public MixinEvents() {
     }
 
+    private static final ResourceLocation LOCATION = new ResourceLocation("hitmarker", "textures/hit.png");
+
     public static void postRenderCrosshair(PoseStack matrices, Minecraft client, int scaledWidth, int scaledHeight) {
         if (HitMarker.remainingTicks > 0) {
-            client.getTextureManager().bind(new ResourceLocation("hitmarker", "textures/hit.png"));
+            RenderSystem.setShaderTexture(0,LOCATION);
             GuiComponent.blit(matrices, (scaledWidth - 11) / 2, (scaledHeight - 11) / 2, 0.0F, 0.0F, 11, 11, 11, 11);
-            client.getTextureManager().bind(Gui.GUI_ICONS_LOCATION);
+            RenderSystem.setShaderTexture(0,Gui.GUI_ICONS_LOCATION);
         }
-
     }
 }
